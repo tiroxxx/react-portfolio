25f81c20570a5c9348160b9703be40cd9297faa0
Christian Marquez <christianmarquez41@gmail.com>
2021-03-26 15:04:20 -0400

    added profile picture

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import { Jumbotron } from "react-bootstrap"
+import { Jumbotron, Image } from "react-bootstrap"
+import PortfolioPicture from "../images/Portfolio-Picture.jpg"
 
 export default function JumbotronComponent() {
     return (
         <>
             <Jumbotron className="bg-transparent">
                 <div className="row">
-                    <div className="col-sm">
-                        <h1>Christian Marquez</h1>
+                    <div className="col-sm align-self-center">
+                        <h1 style={{ fontSize: "4rem"}}>Christian <br/> Marquez</h1>
                         <p>
-                            I am a Full-Stack developer, eager to continue leaning and contribute to big projects.
+                            I am a Full-Stack developer, eager to continue learning and contribute to big projects.
                             Full-time developer, part-time nerd, free-time gamer.
                         </p>
                     </div>
-                    <div className="col-sm">
-                        <img src="./StarWars.JPG" alt="starwars" />
+                    <div className="col-sm mx-auto text-center">
+                        <Image src={PortfolioPicture} alt="Christian Marquez" fluid roundedCircle/>
                     </div>
                 </div>
             </Jumbotron>
